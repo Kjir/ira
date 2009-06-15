aea87890a8ead1bdec53c7f496b5221c8b6df39a
Stéphane Bisinger <stephane.bisinger@gmail.com>
2009-06-15 16:30:23 +0100

    Added command line options to the signal generator

--- test/siggen.cc
+++ test/siggen.cc
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <ipp.h>
+#include <boost/program_options.hpp>
 
 #define SIGLEN 256
 
-int main( void ) {
+int main( int argc, char **argv ) {
+    namespace po = boost::program_options;
+    po::options_description desc("Program options");
+    po::variables_map var_map;
     Ipp16s signal[SIGLEN];
     IppStatus status;
+    IppHintAlgorithm hint;
     float phase = 1.4;
+
+    desc.add_options()
+        ("help", "Print this help message")
+        ("fast", "Use fast algorithm instead of accurate one")
+        ("phase", po::value<float>(&phase)->default_value(0.0), "The starting phase of the signal")
+        ;
+    po::store(po::command_line_parser(argc, argv).options(desc).run(), var_map);
+    po::notify(var_map);
+
+    if( var_map.count("help") ) {
+        std::cout << desc << "\n";
+        return 0;
+    }
+
     ippStaticInit();
-    status = ippsTone_Direct_16s(signal, SIGLEN, 10000, 0.2, &phase, ippAlgHintAccurate);
+    if( var_map.count("fast") ) {
+        hint = ippAlgHintFast;
+    } else {
+        hint = ippAlgHintAccurate;
+    }
+    status = ippsTone_Direct_16s(signal, SIGLEN, 10000, 0.2, &phase, hint);
     if(status != ippStsNoErr) {
         std::cerr << "IPP Error in Tone Direct: " << ippGetStatusString(status) << "\n";
     }
