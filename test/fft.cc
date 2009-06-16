9f98c301091ec915631c51ecbc8c84ef46aeffe8
Stéphane Bisinger <stephane.bisinger@gmail.com>
2009-06-16 12:34:53 +0200

    Added parameters to the FFT

--- test/fft.cc
+++ test/fft.cc
@@ -1,17 +1,52 @@
 #include <iostream>
 #include <ipp.h>
+#include <boost/program_options.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 
-#define ORDER 8
-#define SIGLEN 256
-
-int main( void ) {
-    Ipp16s signal[SIGLEN];
+int main( int argc, char **argv ) {
+    namespace po = boost::program_options;
+    po::options_description desc("Program options");
+    po::variables_map var_map;
+    Ipp16s *signal;
     IppStatus status;
+    IppHintAlgorithm hint;
     IppsFFTSpec_R_16s *FFTSpec;
-    int bufsize;
+    int bufsize, order, siglen, scaling;
     Ipp8u *buffer;
 
-    std::cin.read( (char *)signal, sizeof signal );
+    desc.add_options()
+        ("help", "Print this help message")
+        ("fast", "Use fast algorithm instead of accurate one")
+        ("order,o", po::value<int>(&order)->default_value(8), "Order of the signal. The length of the signal is 2^order")
+        ("order,o", po::value<int>(&scaling)->default_value(0), "Scaling value. The output will be multiplied by 2^-scaling")
+        ("input,i", po::value< std::string >(), "Input file where to get the signal")
+        ("save,s", po::value< std::string >(), "File where to save the signal")
+        ;
+    po::store(po::command_line_parser(argc, argv).options(desc).run(), var_map);
+    po::notify(var_map);
+
+    if( var_map.count("help") ) {
+        std::cout << desc << "\n";
+        return 0;
+    }
+
+    siglen = boost::numeric_cast<int>( pow(2, order) );
+    signal = ippsMalloc_16s(siglen);
+    if( signal == NULL ) {
+	    std::cerr << "Not enough memory" << std::endl;
+	    return -3;
+    }
+
+    if( var_map.count("input") ) {
+	    boost::filesystem::path fp( var_map["input"].as< std::string >() );
+	    boost::filesystem::ifstream file( fp );
+	    file.read( (char *)signal, sizeof signal );
+	    file.close();
+    } else {
+	    std::cin.read( (char *)signal, sizeof signal );
+    }
     std::cerr << "Printing first 10 elements: \n";
     for(int i = 0; i < 10; i++) {
         std::cerr << i << ": " << signal[i] << "\n";
@@ -19,7 +54,12 @@ int main( void ) {
 
 
     ippStaticInit();
-    status = ippsFFTInitAlloc_R_16s(&FFTSpec, ORDER, IPP_FFT_NODIV_BY_ANY, ippAlgHintAccurate);
+    if( var_map.count("fast") ) {
+        hint = ippAlgHintFast;
+    } else {
+        hint = ippAlgHintAccurate;
+    }
+    status = ippsFFTInitAlloc_R_16s(&FFTSpec, order, IPP_FFT_NODIV_BY_ANY, hint);
     if( status != ippStsNoErr ) {
         std::cerr << "IPP Error in InitAlloc: " << ippGetStatusString(status) << "\n";
         return -1;
@@ -35,7 +75,8 @@ int main( void ) {
         std::cerr << "Not enough memory\n";
         return -3;
     }
-    status = ippsFFTFwd_RToPack_16s_ISfs(signal, FFTSpec, 3, buffer);
+
+    status = ippsFFTFwd_RToPack_16s_ISfs(signal, FFTSpec, scaling, buffer);
     if( status != ippStsNoErr ) {
         std::cerr << "IPP Error in FFTFwd: " << ippGetStatusString(status) << "\n";
         return -4;
@@ -46,10 +87,19 @@ int main( void ) {
     buffer = NULL;
 
     std::cerr << "Printing first 10 elements after FFT: \n";
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10 && i < siglen; i++) {
         std::cerr << i << ": " << signal[i] << "\n";
     }
-    std::cout.write( (char *)signal, sizeof signal);
+    if( var_map.count("save") ) {
+	    boost::filesystem::path fp( var_map["save"].as< std::string >() );
+	    boost::filesystem::ofstream file( fp );
+	    file.write((char *)signal, sizeof(*signal) * siglen);
+	    file.close();
+    } else {
+	    std::cout.write((char *)signal, sizeof(*signal) * siglen);
+    }
+    ippFree(signal);
+    signal = NULL;
 
     return 0;
 }
