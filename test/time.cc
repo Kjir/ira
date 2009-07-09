b1e45f7a1c82b3a12cf4ce68fe16fbff98022eda
Stéphane Bisinger <stephane.bisinger@gmail.com>
2009-07-09 16:11:10 +0200

    Separated FFT functions in other source files, started multithreaded implementation

--- test/time.cc
+++ test/time.cc
@@ -1,112 +1,12 @@
 #include <iostream>
 #include <ipp.h>
 #include <boost/program_options.hpp>
-#include <boost/numeric/conversion/cast.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
+#include <boost/numeric/conversion/cast.hpp>
 #include <sys/time.h>
-
-Ipp16s *malloc_16s( int length ) {
-    Ipp16s *mem = ippsMalloc_16s(length);
-    if( mem == NULL ) {
-        std::cerr << "Not enough memory" << std::endl;
-        exit(3);
-    }
-}
-
-void computeFFT(boost::program_options::variables_map &var_map, std::istream *in, IppHintAlgorithm hint, int order, int nint, int scaling, int pscaling) {
-    Ipp16s *signal, *tmpdst, *result;
-    IppStatus status;
-    Ipp8u *buffer;
-    IppsFFTSpec_R_16s *FFTSpec;
-    int bufsize;
-
-    int siglen = boost::numeric_cast<int>( pow(2, order) );
-    result = malloc_16s(siglen);
-    ippsSet_16s(0, result, siglen);
-    signal = malloc_16s(siglen);
-    tmpdst = malloc_16s(siglen);
-
-    status = ippsFFTInitAlloc_R_16s(&FFTSpec, order, IPP_FFT_NODIV_BY_ANY, hint);
-    if( status != ippStsNoErr ) {
-        std::cerr << "IPP Error in InitAlloc: " << ippGetStatusString(status) << "\n";
-        exit(1);
-    }
-    status = ippsFFTGetBufSize_R_16s( FFTSpec, &bufsize );
-    if( status != ippStsNoErr ) {
-        std::cerr << "IPP Error in FFTGetBufSize: " << ippGetStatusString(status) << "\n";
-        exit(2);
-    }
-    buffer = ippsMalloc_8u(bufsize);
-    if( buffer == NULL ) {
-        std::cerr << "Not enough memory\n";
-        exit(3);
-    }
-
-    for( int i = 0; i < nint; i++ ) {
-        (*in).read( (char *)signal, sizeof(*signal) * siglen );
-
-        status = ippsFFTFwd_RToPack_16s_Sfs(signal, tmpdst, FFTSpec, scaling, buffer);
-        if( status != ippStsNoErr ) {
-            std::cerr << "IPP Error in FFTFwd: " << ippGetStatusString(status) << "\n";
-            exit(4);
-        }
-
-        if( var_map.count("power-spectrum")  || nint > 1 ) {
-            Ipp16sc *vc, zero = {0, 0};
-            vc = ippsMalloc_16sc(siglen);
-            if( vc == NULL ) {
-                std::cerr << "Not enough memory\n";
-                exit(3);
-            }
-            //Set the vector to zero
-            ippsSet_16sc(zero, vc, siglen);
-            status = ippsConjPack_16sc(tmpdst, vc, siglen);
-            if( status != ippStsNoErr ) {
-                std::cerr << "IPP Error in ConjPack: " << ippGetStatusString(status) << "\n";
-                exit(5);
-            }
-            status = ippsPowerSpectr_16sc_Sfs(vc, tmpdst, siglen, pscaling);
-            if( status != ippStsNoErr ) {
-                std::cerr << "IPP Error in PowerSpectr: " << ippGetStatusString(status) << "\n";
-                exit(6);
-            }
-            ippsFree(vc);
-            vc = NULL;
-            status = ippsAdd_16s_I(tmpdst, result, siglen);
-            if( status != ippStsNoErr ) {
-                std::cerr << "IPP Error in Add: " << ippGetStatusString(status) << "\n";
-                exit(7);
-            }
-        } else {
-            status = ippsOr_16u_I((Ipp16u *)tmpdst, (Ipp16u *)result, siglen);
-            if( status != ippStsNoErr ) {
-                std::cerr << "IPP Error in Or: " << ippGetStatusString(status) << "\n";
-                exit(8);
-            }
-        }
-    }
-
-    ippFree(signal);
-    ippFree(tmpdst);
-    signal = NULL;
-    tmpdst = NULL;
-    ippsFFTFree_R_16s(FFTSpec);
-    FFTSpec = NULL;
-    ippsFree( buffer );
-    buffer = NULL;
-
-    if( var_map.count("save") ) {
-        boost::filesystem::path fp( var_map["save"].as< std::string >() );
-        boost::filesystem::ofstream file( fp );
-        file.write((char *)result, sizeof(*result) * siglen);
-        file.close();
-    } else {
-        std::cout.write((char *)result, sizeof(*result) * siglen);
-    }
-    ippFree(result);
-    result = NULL;
-}
+#include "fft.h"
+#include "common.h"
 
 int main( int argc, char **argv ) {
     namespace po = boost::program_options;
@@ -115,7 +15,7 @@ int main( int argc, char **argv ) {
     Ipp16s *result;
     IppStatus status;
     IppHintAlgorithm hint;
-    int scaling, pscaling = 13;
+    int scaling, pscaling = 13, siglen = 0;
 
     desc.add_options()
         ("help", "Print this help message")
@@ -152,7 +52,11 @@ int main( int argc, char **argv ) {
     for( int o = 10; o < 27; o++ ) {
         struct timeval start, end;
         gettimeofday(&start, NULL);
-        computeFFT(var_map, in, hint, o, 1, scaling, pscaling);
+        siglen = boost::numeric_cast<int>( pow(2, o) );
+        result = computeFFT(var_map, in, hint, o, 1, scaling, pscaling);
+        write_result(var_map, result, siglen);
+        ippFree(result);
+        result = NULL;
         gettimeofday(&end, NULL);
         double e = ((end.tv_sec - start.tv_sec) * 1000) + (end.tv_usec /1000 - start.tv_usec / 1000);
         order_f << o << '\t' << e << std::endl;
@@ -164,8 +68,16 @@ int main( int argc, char **argv ) {
     (*in).seekg(0);
     for( int o = 10; o < 27; o++ ) {
         struct timeval start, end;
+        /* Time before the FFT */
         gettimeofday(&start, NULL);
-        computeFFT(var_map, in, hint, o, 30, scaling, pscaling);
+
+        siglen = boost::numeric_cast<int>( pow(2, o) );
+        result = computeFFT(var_map, in, hint, o, 30, scaling, pscaling);
+        write_result(var_map, result, siglen);
+        ippFree(result);
+        result = NULL;
+
+        /* Time after the FFT */
         gettimeofday(&end, NULL);
         double e = ((end.tv_sec - start.tv_sec) * 1000) + (end.tv_usec /1000 - start.tv_usec / 1000);
         orderint_f << o << '\t' << e << std::endl;
@@ -175,10 +87,16 @@ int main( int argc, char **argv ) {
     boost::filesystem::path int_fp( "integration.dat" );
     boost::filesystem::ofstream int_f( int_fp );
     (*in).seekg(0);
+    siglen = boost::numeric_cast<int>( pow(2, 15) );
     for( int i = 20; i < 3000; i += 30 ) {
         struct timeval start, end;
         gettimeofday(&start, NULL);
-        computeFFT(var_map, in, hint, 15, i, scaling, pscaling);
+
+        result = computeFFT(var_map, in, hint, 15, i, scaling, pscaling);
+        write_result(var_map, result, siglen);
+        ippFree(result);
+        result = NULL;
+
         gettimeofday(&end, NULL);
         double e = ((end.tv_sec - start.tv_sec) * 1000) + (end.tv_usec /1000 - start.tv_usec / 1000);
         int_f << i << '\t' << e << std::endl;
@@ -194,7 +112,13 @@ int main( int argc, char **argv ) {
     for( int o = 10; o < 27; o++ ) {
         struct timeval start, end;
         gettimeofday(&start, NULL);
-        computeFFT(var_map, in, hint, o, 1, scaling, pscaling);
+
+        siglen = boost::numeric_cast<int>( pow(2, o) );
+        result = computeFFT(var_map, in, hint, o, 1, scaling, pscaling);
+        write_result(var_map, result, siglen);
+        ippFree(result);
+        result = NULL;
+
         gettimeofday(&end, NULL);
         double e = ((end.tv_sec - start.tv_sec) * 1000) + (end.tv_usec /1000 - start.tv_usec / 1000);
         nomulti_f << o << '\t' << e << std::endl;
@@ -210,7 +134,13 @@ int main( int argc, char **argv ) {
     for( int o = 10; o < 27; o++ ) {
         struct timeval start, end;
         gettimeofday(&start, NULL);
-        computeFFT(var_map, in, hint, o, 1, scaling, pscaling);
+
+        siglen = boost::numeric_cast<int>( pow(2, o) );
+        result = computeFFT(var_map, in, hint, o, 1, scaling, pscaling);
+        write_result(var_map, result, siglen);
+        ippFree(result);
+        result = NULL;
+
         gettimeofday(&end, NULL);
         double e = ((end.tv_sec - start.tv_sec) * 1000) + (end.tv_usec /1000 - start.tv_usec / 1000);
         multi5_f << o << '\t' << e << std::endl;
